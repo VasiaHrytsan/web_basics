ef9c065f9dda1bbd9f1fd0f6fa045ab034a9a896
Vasia <vgrican48@gmail.com>
2024-10-16 15:20:28 +0300

    commit 2

---
+++ Lab4/script.js
@@ -0,0 +1,13 @@
+function clearInput() {
+    document.getElementById("_name").value = null;
+    document.getElementById("_date").value = null;
+    document.getElementById("_mail").value = null;
+    document.getElementById("_messege").value = null;
+    document.getElementById("coffee").checked = false;
+    document.getElementById("tea").checked = false;
+    document.getElementById("water").checked = false;
+    document.getElementById("fanta").checked = false;
+    document.getElementById("UA").checked = false;
+    document.getElementById("USA").checked = false;
+    document.getElementById("GE").checked = false;
+}
